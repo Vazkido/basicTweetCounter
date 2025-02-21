a419c35a766e689fc15d658af177e95c62f56274
vazkido <vazkid02@gmail.com>
2025-02-21 18:04:35 -0500

    js update 1

--- tweet.js
+++ tweet.js
@@ -1,3 +1,9 @@
-const tweet = prompt('What is your tweet?');
+const tweet = prompt("What is your tweet?");
 const tweetCount = tweet.length;
-alert("You have inserted " + )
+alert(
+  "You have inserted " +
+    tweetCount +
+    " and you have " +
+    (240 - tweetCount) +
+    " texts remaining."
+);
