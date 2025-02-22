4bc89754eae2c43c1411564e7100cfdadcdef1e1
vazkido <vazkid02@gmail.com>
2025-02-22 17:45:26 -0500

    js update first trial

--- tweet.js
+++ tweet.js
@@ -1,18 +1,28 @@
-import { text } from "body-parser";
-import { log } from "console";
+// import { text } from "body-parser";
+// import { log } from "console";
 
-const tweet = prompt("Enter is your tweet?");
-const tweetCount = tweet.length;
-alert(
-  "You have inserted " +
-    tweetCount +
-    " texts and you have " +
-    (240 - tweetCount) +
-    " texts remaining."
-    if (tweetCount >= 240) {
-        alert("You have no inputs.");
-    }
-    else {
-        alert("")
-    }
-);
+//   FIRST TRIAL
+
+
+// const tweet = prompt("Enter your tweet?");
+// const tweetCount = tweet.length;
+// alert(
+//   "You have inserted " +
+//     tweetCount +
+//     " texts and you have " +
+//     (240 - tweetCount) +
+//     " texts remaining."
+
+//     if (tweetCount >= 240) {
+//         alert("You have no inputs.");
+//     }
+//     else {
+//         alert("Input accepted")
+//     }
+// );
+
+//    END
+
+
+// CORRECTION
+
