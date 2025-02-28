d337f40b627db48b8eae676b722c3f7e468101d5
vazkido <vazkid02@gmail.com>
2025-02-28 16:59:39 -0500

    app.use

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
 import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
+
+dotenv.config();
+const app = express();
+
+app.use(express.json()); // parse JSON body
+app.use(cors()); // Allow frontend requests
