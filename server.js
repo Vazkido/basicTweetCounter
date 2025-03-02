b000a529e9ba60b3024ea7b9e8b18a867da5abde
vazkido <vazkid02@gmail.com>
2025-03-02 17:50:36 -0500

    added codes for start server

--- server.js
+++ server.js
@@ -21,4 +21,7 @@ app.post("/tweet,", (req, res) => {
 });
 
 // Start Server
-const PORT 
+const PORT = process.env.PORT || 8081;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
